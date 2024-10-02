05e513c24edbae63500f1834aa05dd4735e2906d
TwoBrake <Lucas_Stranks@outlook.com>
2024-10-01 18:06:48 -0700

    fix bugs make socket actually work

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export class pteroWebSocket {
   // Internal variables
   private socketUrl: string;
   private socketToken: string;
+  private socket: WebSocket;
 
   constructor(panelUrl: string, clientKey: string, serverId: string) {
     this.panelUrl = panelUrl;
@@ -18,29 +19,71 @@ export class pteroWebSocket {
 
     try {
       (async () => {
-        const socketReq = await axios.get(
-          `${this.panelUrl}/api/client/servers/${this.serverId}/websocket`,
-          {
-            headers: {
-              Authorization: `Bearer: ${this.clientKey}`,
-            },
-          }
-        );
+        await this.authCon(true);
 
-        this.socketUrl = socketReq.data.data.socketUrl;
-        this.socketToken = socketReq.data.data.token;
+        this.socket.on("auth success", () => {
+          console.log("Successfully authenticated connection to websocket.");
+        });
+
+        this.socket.on("token expiring", async () => {
+          await this.authCon(false);
+        });
       })();
     } catch (e) {
       throw e;
     }
   }
 
-  public on(event: string, func: (args: []) => void) {
-    // TODO: Once websocket connection is actually made, integrate it with the "on" event.
-    const testEvent = "msg";
+  /**
+   * Listens to a specific event.
+   * @param event - The event to listen to.
+   * @param func - The function to run when the event is triggered.
+   */
+  public on(event: string, func: (args: any) => void): void {
+    if (!this.socket)
+      throw new Error("Websocket connection has not yet been established.");
 
-    if (testEvent === event) {
-      func([]);
+    this.socket.on(event, (args) => func(args));
+  }
+
+  /**
+   * Connects and authenticates or reauthenticate on token expiring the websocket connection.
+   * @param initial - Whether or not this is the initial connection to the websocket.
+   */
+  private async authCon(initial: boolean): Promise<void> {
+    const socketReq = await axios.get(
+      `${this.panelUrl}/api/client/servers/${this.serverId}/websocket`,
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.clientKey}`,
+        },
+      }
+    );
+
+    this.socketToken = socketReq.data.data.token;
+
+    if (initial) {
+      this.socketUrl = socketReq.data.data.socket;
+
+      this.socket = new WebSocket(this.socketUrl, {
+        headers: {
+          authorization: this.socketToken,
+        },
+        origin: this.panelUrl,
+      });
+
+      this.socket.on("open", () => {
+        console.log("Connected to websocket.");
+        this.socket.send(
+          JSON.stringify({ event: "auth", args: [this.socketToken] }) // Send packet to websocket to authenticate with websocket token.
+        );
+      });
+    } else {
+      this.socket.send(
+        JSON.stringify({ event: "auth", args: [this.socketToken] }) // Revalidate socket token.
+      );
     }
   }
 }
--- src/test/class.ts
+++ src/test/class.ts
@@ -1,4 +1,17 @@
 // Resources
 import { pteroWebSocket } from "..";
+import dotenv from "dotenv";
 
-const socket = new pteroWebSocket("https://test.com", "test", "1");
+dotenv.config(); // Make sure environment variables are loaded correctly.
+
+const socket = new pteroWebSocket(
+  "https://panel.twobrake.xyz",
+  process.env.CLIENT_KEY as string,
+  "5bc8b493"
+);
+
+setTimeout(() => {
+  socket.on("message", (msg) => {
+    console.log(JSON.parse(msg.toString("utf-8")));
+  });
+}, 1000);
